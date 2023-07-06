59b3967c439fae04c8254cc23e25fca943a87b94
Viciouz Sholensk <cdmx2418@amerike.edu.mx>
2023-07-05 18:55:35 -0600

    Movimiento basico
    Ya esta el movimiento en 8 direcciones.

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+
+    private Rigidbody2D _rb;
+    private Vector2 _moveInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        _moveInput = new Vector2(moveX, moveY).normalized;
+    }
+    
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + _moveInput * (speed * Time.fixedDeltaTime));
+    }
+}
